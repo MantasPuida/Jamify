109a65557c908ffc06c513642c3c6b2cfbbf3b37
Mantas <mantasxpuida@gmail.com>
2022-02-26 15:33:37 +0200

    additional fixes

--- src/app/Home/home-landing-page.tsx
+++ src/app/Home/home-landing-page.tsx
@@ -6,31 +6,49 @@ import { HeaderComponent } from "./header/header-component";
 import { HomeLandingPageStyles, useHomeLandingPageStyles } from "./landing-page.styles";
 import { FeaturedPlaylists } from "./featured-playlists/featured-playlists";
 import { YoutubePlaylists } from "../youtube-playlists/playlist-component";
+import { useYoutubeAuth } from "../../context/youtube-context";
+import { useDeezerAuth } from "../../context/deezer-context";
+import { useSpotifyAuth } from "../../context/spotify-context";
 
 interface OuterProps {
   spotifyApi: SpotifyWebApi;
 }
 
-type InnerProps = WithStyles<typeof HomeLandingPageStyles>;
+interface InnerProps extends WithStyles<typeof HomeLandingPageStyles> {
+  spotifyToken: string | null;
+  deezerToken: string | null;
+  youtubeToken: string | null;
+}
 
 type Props = InnerProps & OuterProps;
 
 class HomeLandingPageClass extends React.PureComponent<Props> {
   public render(): React.ReactNode {
-    const { classes, spotifyApi } = this.props;
+    const { classes, spotifyApi, spotifyToken, youtubeToken } = this.props;
 
     return (
       <Grid container={true} item={true} xs={12} className={classes.homeGrid}>
         <HeaderComponent spotifyApi={spotifyApi} />
-        <FeaturedPlaylists spotifyApi={spotifyApi} />
-        <YoutubePlaylists />
+        {spotifyToken && <FeaturedPlaylists spotifyApi={spotifyApi} />}
+        {youtubeToken && <YoutubePlaylists />}
       </Grid>
     );
   }
 }
 
 export const HomeLandingPage = React.memo<OuterProps>((props) => {
+  const { youtubeToken } = useYoutubeAuth();
+  const { deezerToken } = useDeezerAuth();
+  const { spotifyToken } = useSpotifyAuth();
   const classes = useHomeLandingPageStyles();
 
-  return <HomeLandingPageClass {...props} classes={classes} />;
+  return (
+    <HomeLandingPageClass
+      {...props}
+      classes={classes}
+      youtubeToken={youtubeToken}
+      deezerToken={deezerToken}
+      spotifyToken={spotifyToken}
+    />
+  );
 });
